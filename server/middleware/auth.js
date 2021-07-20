4243b3ac286a1f9e15a416ee1008c524c454a48c
unknown <ankit.singhania767@gmail.com>
2021-07-21 03:09:42 +0530

    added try and catch block

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,36 +1,44 @@
-const dotenv = require("dotenv");
-const jwt = require("jsonwebtoken");
-const Users = require("../models/user.model");
+const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
+const Users = require('../models/user.model')
 
-dotenv.config();
-const SECRET_KEY = process.env.SECRET_KEY;
+dotenv.config()
+const SECRET_KEY = process.env.SECRET_KEY
 const verifyToken = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization.split(' ')[1]
+  console.log({ token })
+  try {
+    const { userId } = jwt.verify(token, SECRET_KEY)
+    if (userId) {
+      const user = await Users.findById(userId)
 
-  const { userId } = jwt.verify(token, SECRET_KEY);
-  if (userId) {
-    const user = await Users.findById(userId);
-
-    if (user) {
-      req.user = user;
-      req.token = token;
-      req.userId = userId;
-      next();
+      if (user) {
+        req.user = user
+        req.token = token
+        req.userId = userId
+        next()
+      } else {
+        res.json({ success: false, message: 'Invalid User' })
+      }
     } else {
-      res.json({ success: false, message: "Invalid User" });
+      res.json({ success: false, message: 'Invalid Token' })
     }
-  } else {
-    res.json({ success: false, message: "Invalid Token" });
+  } catch (error) {
+    res.status(401).json({ success: false, message: error.message })
   }
-};
+}
 
 const createToken = async (req, res, next) => {
-  const { userId } = req;
-  const token = jwt.sign({ userId }, SECRET_KEY, {
-    expiresIn: "365d",
-  });
-  req.token = token;
-  next();
-};
+  const { userId } = req
+  try {
+    const token = jwt.sign({ userId }, SECRET_KEY, {
+      expiresIn: '365d',
+    })
+    req.token = token
+    next()
+  } catch (error) {
+    res.status(401).json({ success: false, message: error.message })
+  }
+}
 
-module.exports = { verifyToken, createToken };
+module.exports = { verifyToken, createToken }
