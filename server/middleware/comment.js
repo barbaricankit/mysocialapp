ad60178aaf8849b39aad96367b5d001c8f188d21
unknown <ankit.singhania767@gmail.com>
2021-08-05 23:39:35 +0530

    changed logic for comment notification

--- server/middleware/comment.js
+++ server/middleware/comment.js
@@ -1,36 +1,50 @@
-const Comments = require("../models/comment.model");
-const Posts =require("../models/post.model")
+const Comments = require('../models/comment.model')
+const Posts = require('../models/post.model')
 const addNewComment = async (req, res, next) => {
-  const { user, postId } = req;
+  const { user, postId } = req
 
-  const { description } = req.body;
+  const { description } = req.body
 
   const newComment = new Comments({
     user: user._id,
     description,
     post: postId,
-  });
-  const post = await Posts.findById(postId);
-  post.comments += 1;
-  req.postId = postId;
-  await newComment.save();
-  await post.save();
-  if(post.user===user._id)
-  {
-    const comment = await newComment.populate("user").execPopulate();
-    comment.user.password = undefined;
-    res.json({ success: true, comments: comment });
-  }else{
-    req.comment = newComment;
-  req.post=post
-  next();}
-};
+  })
+  const post = await Posts.findById(postId)
+  post.comments += 1
+  req.postId = postId
+  await newComment.save()
+  await post.save()
+  if (post.user.toString() === user._id.toString()) {
+    req.isPostUser = true
+    req.comment = newComment
+    req.post = post
+    next()
+  } else {
+    req.comment = newComment
+    req.post = post
+    req.isPostUser = false
+    next()
+  }
+}
 
 const commentNotification = async (req, res, next) => {
-  const { post} = req;
-  req.notifiedUserIds = [post.user];
-  req.notification_type = "COMMENT_TYPE";
-  next();
-};
+  const { user, postId, post, isPostUser, comment } = req
+  const comments = await Comments.find({ post: postId })
+  const userIds = comments.filter((comment) => comment.user !== user._id)
+  if (isPostUser) {
+    if (userIds.length) {
+      const comments = await comment.populate('user').execPopulate()
+      comments.user.password = null
+      res.json({ success: true, comments })
+    } else {
+      req.notifiedUserIds = userIds
+      next()
+    }
+  } else {
+    req.notifiedUserIds = [...userIds, post.user]
+    next()
+  }
+}
 
-module.exports = { addNewComment, commentNotification };
+module.exports = { addNewComment, commentNotification }
